86598193c6ce28db5dc9deff0d033148247f5650
이명진 <88179341+thingineeer@users.noreply.github.com>
2022-08-30 23:25:51 +0900

    2020_kakao_intern_level1

---
+++ 2022_kakao.py
@@ -0,0 +1,23 @@
+# 성격 유형 검사하기
+
+def solution(survey, choices):
+    score = {1:3, 2:2, 3:1, 4:0, 5:1, 6:2, 7:3}
+    mid = 4
+    list1 = ['R','T','C','F','J','M','A','N']
+    list2 = [ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ]
+    answer = ''
+    
+    for i in range(len(choices)):
+        if mid < choices[i]:
+            list2[list1.index(survey[i][1])] += score[choices[i]]
+        elif mid > choices[i]:
+            list2[list1.index(survey[i][0])] += score[choices[i]]
+        else:
+            continue
+            
+    for i in range(0, 8, 2):
+        if list2[i] < list2[i+1]:
+            answer += list1[i+1]
+        else:
+            answer += list1[i]
+    return answer
