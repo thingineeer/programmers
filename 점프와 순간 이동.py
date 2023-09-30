8c9cc29bb15f32c03bd5886595fecf976c349602
이명진 <88179341+thingineeer@users.noreply.github.com>
2023-10-01 03:45:10 +0900

    Summer/Winter Coding(~2018)
    구현
    이진수의 1의 개수로 푼 사람이 있는데 존경 스럽다.

---
+++ 점프와 순간 이동.py
@@ -0,0 +1,11 @@
+def solution(n):
+    result = 0
+
+    while n != 0:
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n -= 1
+            result += 1
+            
+    return result
